41cc4c930293dbf0ee0d4165cc2e72adb294d58f
Alexandre Santos <amenezes031@gmail.com>
2021-07-22 15:59:05 -0300

    add typing to Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,16 @@
-const Layout: React.FC = ({ children}) => {
+import { ReactNode } from "react";
+import Header from "./Header";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">{children}</div>
+    <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
+      <Header />
+      {children}
+    </div>
   );
 }
 
