abe7783619bb0383f4d9b4417f07341f3b8ede8c
Alexandre Santos <amenezes031@gmail.com>
2021-07-22 15:59:35 -0300

    create NavLink component

---
+++ src/components/NavLink.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+interface NavLinkProps {
+  href: string;
+  description: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, description }) => {
+  return (
+    <Link  href={href}>
+      <a className="tracking-wide hover:text-gray-300">{description}</a>
+    </Link>
+  );
+}
+
+export default NavLink;
