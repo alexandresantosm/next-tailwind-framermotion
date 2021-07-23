d214aae9b2f46a10b7ed0812762322eda51e1022
Alexandre Santos <amenezes031@gmail.com>
2021-07-22 22:29:44 -0300

    create Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+interface CardProps {
+  href: string;
+  src: string;
+  alt: string;
+  layoutId: string;
+}
+
+const Card: React.FC<CardProps> = ({ href, src, alt, layoutId }) => {
+  return (
+    <Link href={href}>
+      <div className="relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition">
+        <motion.img src={src} alt={alt} layoutId={layoutId} className="w-full h-full"/>
+      </div>
+    </Link>
+  );
+}
+
+export default Card;
