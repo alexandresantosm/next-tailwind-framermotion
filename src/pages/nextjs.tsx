25b09a925768978fb6d44e0da089672e017091a2
Alexandre Santos <amenezes031@gmail.com>
2021-07-23 14:31:50 -0300

    update Nextjs component

--- src/pages/nextjs.tsx
+++ src/pages/nextjs.tsx
@@ -1,18 +1,29 @@
 import Link from 'next/link';
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 const NextjsPage: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
-      <motion.img src="/images/nextjs.svg" alt="Next.js" layoutId="nextjs-logo" transition={{ duration: 0.5 }} />
+      <motion.img
+        src="/images/nextjs.svg"
+        alt="Next.js"
+        layoutId="nextjs-logo"
+        transition={{ duration: 0.5 }}
+      />
 
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1, duration: 1 }}>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 1 }}
+      >
         <Link href="/">
-          <a className="bg-gray-900 text-gray-50 px-6 py-6 text-lg font-semibold rounded-xl hover:bg-gray-700 transition">Voltar para a Home</a>
+          <a className="bg-gray-900 text-gray-50 px-6 py-3 text-lg font-semibold rounded-xl hover:bg-gray-700 transition">
+            Voltar para a Home
+          </a>
         </Link>
       </motion.div>
     </div>
   );
-}
+};
 
 export default NextjsPage;
