0e5f84f395e055fd83c89d38c3e8ea472d65086e
Alexandre Santos <amenezes031@gmail.com>
2021-07-22 21:42:05 -0300

    create title home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <h1>Logo</h1>
+      <h1 className="text-4xl sm:text-6xl md:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">Construa sites modernos com agilidade sem precisar sair do HTML.</h1>
     </Layout>
   )
 }
